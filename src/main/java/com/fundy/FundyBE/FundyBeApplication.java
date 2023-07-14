cedba41c7089d617e5c911fa82749c964a14b950
EastWon0103 <dongwon000103@gmail.com>
2023-07-14 17:58:51 +0900

    init project

---
+++ src/main/java/com/fundy/FundyBE/FundyBeApplication.java
@@ -0,0 +1,13 @@
+package com.fundy.FundyBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundy/FundyBE/FundyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundy.FundyBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
