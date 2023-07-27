d101417abe12f0d917b8257af4f8a496ad460317
EastWon0103 <dongwon000103@gmail.com>
2023-07-27 13:21:42 +0900

    feat: 유저 정보 조회

---
+++ src/main/java/com/fundy/FundyBE/global/exception/customException/NoUserException.java
@@ -0,0 +1,15 @@
+package com.fundy.FundyBE.global.exception.customException;
+
+public class NoUserException extends RuntimeException{
+    private NoUserException(String message) {
+        super(message);
+    }
+
+    public final static NoUserException createBasic() {
+        return new NoUserException("유저가 존재하지 않습니다");
+    }
+
+    public final static NoUserException withCustomMessage(String message) {
+        return new NoUserException(message);
+    }
+}
