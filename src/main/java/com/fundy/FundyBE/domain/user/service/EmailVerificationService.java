9b8580f1d4c61a11c82c6b5e0f1b2528d6b5e56a
EastWon0103 <dongwon000103@gmail.com>
2023-08-28 21:30:17 +0900

    refactor: EmailSender

--- src/main/java/com/fundy/FundyBE/domain/user/service/EmailVerificationService.java
+++ src/main/java/com/fundy/FundyBE/domain/user/service/EmailVerificationService.java
@@ -7,9 +7,15 @@ import com.fundy.FundyBE.global.component.email.AsyncEmailSender;
 import com.fundy.FundyBE.global.component.jwt.EmailVerifyJwtProvider;
 import com.fundy.FundyBE.global.component.jwt.dto.request.VerifyEmailInfoRequest;
 import com.fundy.FundyBE.global.validation.user.UserValidator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import java.util.Random;
 
@@ -20,12 +26,14 @@ public class EmailVerificationService {
     private final AsyncEmailSender asyncEmailSender;
     private final EmailVerifyJwtProvider emailVerifyJwtProvider;
     private final UserValidator userValidator;
+    private final TemplateEngine templateEngine;
+    private final JavaMailSender javaMailSender;
 
     public EmailCodeResponse sendEmailCodeAndReturnToken(String email) {
         userValidator.hasDuplicateEmail(email);
         String code = generateCode();
         String token = emailVerifyJwtProvider.generateToken(email, code);
-        asyncEmailSender.sendEmailCode(email, code);
+        sendEmailCode(email, code);
 
         return EmailCodeResponse.builder()
                 .email(email)
@@ -55,5 +63,28 @@ public class EmailVerificationService {
             code += numericCharacters.charAt(random.nextInt(numericCharacters.length()));
         }
         return code;
+    }
+
+    private void sendEmailCode(String email, String code) {
+        try {
+            asyncEmailSender.sendEmail(createEmailCodeMessage(email, code));
+        } catch (MessagingException e) {
+            log.warn("이메일 전송에 실패하였습니다.");
+        }
+    }
+
+    private MimeMessage createEmailCodeMessage(String email, String code) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        message.setSubject("이메일 인증 코드");
+        message.addRecipients(Message.RecipientType.TO, email);
+        message.setText(getEmailCodeContext(email, code), "utf-8", "html");
+        return message;
+    }
+
+    private String getEmailCodeContext(String email, String code) {
+        Context context = new Context();
+        context.setVariable("email", email);
+        context.setVariable("code", code);
+        return templateEngine.process("sendCode",context);
     }
 }
--- src/main/java/com/fundy/FundyBE/global/component/email/AsyncEmailSender.java
+++ src/main/java/com/fundy/FundyBE/global/component/email/AsyncEmailSender.java
@@ -1,48 +1,19 @@
 package com.fundy.FundyBE.global.component.email;
 
-import jakarta.mail.Message;
-import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class AsyncEmailSender {
     private final JavaMailSender javaMailSender;
-    private final TemplateEngine templateEngine;
-
     @Async("ThreadPoolTaskExecutor")
-    public void sendEmailCode(String email, String token) {
-        MimeMessage message = javaMailSender.createMimeMessage();
-        try {
-
-            message.setSubject("이메일 인증 코드");
-            message.addRecipients(
-                    Message.RecipientType.TO, email);
-            message.setText(
-                    getEmailCodeContext(email,token),
-                    "utf-8",
-                    "html"
-            );
-            javaMailSender.send(message);
-        } catch (MessagingException e) {
-            log.warn("이메일 전송 실패");
-            e.printStackTrace();
-        }
+    public void sendEmail(MimeMessage message) {
+        javaMailSender.send(message);
     }
-
-    private String getEmailCodeContext(String email, String code) {
-        Context context = new Context();
-        context.setVariable("email", email);
-        context.setVariable("code", code);
-        return templateEngine.process("sendCode",context);
-    }
-
 }
